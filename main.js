8b5d969c631e222b23c72a73d83f1e06451e9331
blanche <79345528+cd-blanche@users.noreply.github.com>
2023-03-02 20:49:42 -0500

    Add mini todo list + minor fixes

--- main.js
+++ main.js
@@ -3,23 +3,19 @@
 document.addEventListener('mousedown', (e) => {
 
   if (e.target.className === 'window-header') {
-    // grab mouse position
     getMousePos(e);
-    // stop grabbing on mouse up
     document.onmouseup = stopDrag;
   };
 });
 
 function getMousePos(element) {
-  // main window to be moved
-  const main = element.target.parentNode;
+  const currentWindow = element.target.parentNode;
   const mouseY = element.offsetY;
   const mouseX = element.offsetX;
 
-  // set window position based on mouse position
   document.onmousemove = e => {
-    main.style.top = (e.y - mouseY) + 'px';
-    main.style.left = (e.x - mouseX) + 'px';
+    currentWindow.style.top = (e.y - mouseY) + 'px';
+    currentWindow.style.left = (e.x - mouseX) + 'px';
   };
 };
 
@@ -27,3 +23,6 @@ function stopDrag() {
   document.onmousemove = null;
   document.onmouseup = null;
 };
+
+// to do
+// prevent window from hitting edges and navigation bar
